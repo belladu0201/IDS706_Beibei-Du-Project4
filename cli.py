08d2b5099c807ac128e1dd3424c5df7a4652187a
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-11-28 05:31:46 +0000

    add

--- cli.py
+++ cli.py
@@ -1,20 +1,23 @@
 import click
 import statistics
+import pandas as pd
 from mylib.runhealth import health_generator
+
+df = pd.read_csv('fetal_health.csv')
 
 
 @click.command()
-@click.option("--health", help="Health status to add")
-def statushealth(df):
+#@click.option("--health", help="Health status to add")
+def statushealth():
     """Pass in additional fruit"""
 
     chosen_random_ten = health_generator(df)
     click.echo(
         click.style(
             f"Your choice was randomly selected: {statistics.mode(set(chosen_random_ten))}", fg="red", bold=True
-        )
+        ))
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
+    #pylint: disable=no-value-for-parameter
     statushealth()
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from fastapi import FastAPI
 import uvicorn
 from mylib.runhealth import health_generator
 import statistics
+import pandas as pd
+
+df = pd.read_csv('fetal_health.csv')
 
 app = FastAPI()
 
@@ -13,13 +16,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hello IDS706! This is a Repo for IDS706 Project 4!"}
 
-@app.get("/health/{health}")
-async def healthstatus(df: dataframe):
-    """Adds a fruit to random fruit"""
-
+@app.get("/")
+async def healthstatus():
     chosen_random_ten = health_generator(df)
     output = statistics.mode(set(chosen_random_ten))
-    return {"mode_health_status": chosen_random_ten}
+    return {"mode_health_status": output}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
