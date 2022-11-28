4d1ec985d51ecd71ea60f1e88b6eb628b0109d0b
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-11-28 00:02:03 -0500

    Update runhealth.py

--- mylib/runhealth.py
+++ mylib/runhealth.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 df = pd.read_csv('fetal_health.csv')
 
+# randomly select 10 numbers
 def random_num(df):
 
-    random_number = choices(range(0,df.shape[0]), k=1)
+    random_number = choices(range(0,df.shape[0]), k=10)
 
     return random_number
-
+# return a list of fetal health status
 def health_generator(df):
     """Returns back random health fetal status"""
 
     num = random_num(df)
     health = df.iloc[num, 21]
-    return health.values[0]
+    return list(health.values[:10])
