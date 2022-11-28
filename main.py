0c98828050369023a9bb2560b7de1dac560c952f
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-11-28 00:12:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import json
 from fastapi import FastAPI
 import uvicorn
+from mylib.runhealth import health_generator
+import statistics
 
 app = FastAPI()
 
@@ -11,6 +13,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello IDS706! This is a Repo for IDS706 Project 4!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/health/{health}")
+async def healthstatus(df: dataframe):
+    """Adds a fruit to random fruit"""
+
+    chosen_random_ten = health_generator(df)
+    output = statistics.mode(set(chosen_random_ten))
+    return {"mode_health_status": chosen_random_ten}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
